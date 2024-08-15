33aa5ef2fe2bc3cf35cb9269841bb4e1659a2dc5
udhayy007 <153197945+udhayy007@users.noreply.github.com>
2024-08-15 22:47:17 +0530

    Add files via upload

---
+++ ASSIGNMENT7/assignment7.js
@@ -0,0 +1,82 @@
+// what is JavaScript
+
+/*JavaScript is a programming language commonly used to create interactive effects within web browsers.
+ It allows developers to implement complex features on web pages, such as dynamically updating content, 
+ controlling multimedia, and animating images.*/
+  
+
+ // The Above is the answer of question 1 and 3
+
+
+let userAge = 25;
+
+let userName = "Udhaya Nithi";
+
+
+console.log("User Name:", userName);
+console.log("User Age:", userAge);
+
+/*---------------------------------------------------------------------------------------------------*/
+
+let num1 = 10;
+let num2 = 5;
+
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
+
+/*---------------------------------------------------------------------------------------------------*/
+
+let myString = "Hello, World!";
+console.log("String:", myString);
+
+let myNumber = 42;
+console.log("Number:", myNumber);
+
+let myBoolean = true;
+console.log("Boolean:", myBoolean);
+
+let myArray = [1, 2, 3, 4, 5];
+console.log("Array:", myArray);
+
+/*---------------------------------------------------------------------------------------------------*/
+
+function greetUser(name) {
+    return "Hello, " + name + "!";
+}
+
+console.log(greetUser("udhay"));
+
+/*---------------------------------------------------------------------------------------------------*/
+
+let temperature = 35;
+
+if (temperature > 30) {
+    console.log("It's hot outside!");
+} else {
+    console.log("The temperature is moderate.");
+}
+
+/*---------------------------------------------------------------------------------------------------*/
+
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+console.log(5 == '5'); 
+
+console.log(5 === '5'); 
+
+
+
+
+
+
+
